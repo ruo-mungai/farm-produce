e180555c604a86cf2cab3ef11f9b58a9a6f70b39
Patrick <ruomungai@gmail.com>
2022-09-06 10:58:29 +0300

    create farmer table

--- backend/db/migrate/20220906074911_create_farmer.rb
+++ backend/db/migrate/20220906074911_create_farmer.rb
@@ -1,4 +1,9 @@
 class CreateFarmer < ActiveRecord::Migration[6.1]
   def change
+    create_table :farmers do |t|
+      t.string :name
+      t.string :tel
+      t.string :location
+    end
   end
 end
