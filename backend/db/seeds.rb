b07ffad35d889184842ea6331416e6cdb2a85d12
Patrick <ruomungai@gmail.com>
2022-09-06 12:51:12 +0300

    creating seed file

---
+++ backend/db/seeds.rb
@@ -0,0 +1,25 @@
+puts 'seedin started .......'
+
+ f1 = Farmer.create(
+    name: "Jamta plantation", tel: "0712 546 987",
+    location: "Lamata Estate"
+ )
+ f2 = Farmer.create(
+    name: "Penniata plantation", tel: "0722 564 978",
+    location: "Pennita Estate"
+ )
+ f3 = Farmer.create(
+    name: "Patiana plantation", tel: "0720 564 978",
+    location: "Patiana Estate"
+ )
+
+ c1=Category.create(name: "Animal Product")
+ c2=Category.create(name: "Garden Products")
+
+ p1=Product.create(name: "Milk", quantity: 30, price: 1800,
+category_id: c1, farmer_id: f2)
+ p2=Product.create(name: "Milk", quantity: 40, price: 2400,
+category_id: c1, farmer_id: f3)
+ p3=Product.create(name: "Onion", quantity: 10, price: 1000,
+category_id: c2, farmer_id: f1)
+
