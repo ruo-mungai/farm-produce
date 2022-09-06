6e6ebeb5e36a21cf03340c33ef47e7eba0b28eaf
Patrick <ruomungai@gmail.com>
2022-09-06 11:02:40 +0300

    creating products table

--- backend/db/migrate/20220906075216_create_product.rb
+++ backend/db/migrate/20220906075216_create_product.rb
@@ -1,4 +1,9 @@
 class CreateProduct < ActiveRecord::Migration[6.1]
   def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :quantity
+      t.integer :price
+    end
   end
 end
